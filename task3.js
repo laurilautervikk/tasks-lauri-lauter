1ccd9f2fa51be347a6d9eb43be361afc64ade7b0
laurilautervikk <lauri.lauter@vikk.ee>
2021-09-08 14:52:15 +0300

    3_2 half way

--- task3.js
+++ task3.js
@@ -5,7 +5,6 @@ function greet(name) {
         //console.log('<asukoht array>')
         return 'Hello, ' + name[0] + ' and ' +name[1] + '.';
         
-
     } else if (name != null) {
         //console.log('<asukoht name>')
         return 'Hello, ' + name + '.';
@@ -13,11 +12,9 @@ function greet(name) {
     } else {
         //console.log('<asukoht friend>')
         return 'Hello, my friend.';
-
     }
 
 };
-
 
 
 
---
+++ task3_2.js
@@ -0,0 +1,28 @@
+
+function greet(name) {
+
+    let result = 'Hello, ';
+    let row = '';
+
+    if  (Array.isArray(name)) {
+
+        row.push();
+        //result += name[0] + ' and ' +name[1];
+
+    } else if (name) {
+        result +=  name;
+
+    } else {
+        result +=  'my friend';
+
+    }
+    return result + row + '.';
+
+};
+
+
+
+console.log(greet('Bob'));
+console.log(greet());
+console.log(greet(['Jill', 'Jane']));
+console.log(greet(['Jill', 'Jane', 'Judy'])); // Make this work
