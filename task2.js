6cffa3082f8783aeceecba0313b5467bda77dc6a
laurilautervikk <lauri.lauter@vikk.ee>
2021-09-08 13:21:00 +0300

    initial

---
+++ task2.js
@@ -0,0 +1,20 @@
+let eesnimi ='Lauri';
+let sünnipäev ='6';
+let sünnikuu ='Oktoober';
+let sünniaasta ='1980';
+let hobid = ['ballett','voolimine'];
+let object = {
+    eesnimi:eesnimi,
+    sünnipäev:sünnipäev,
+    sünnikuu:sünnikuu,
+    sünniaasta:sünniaasta,
+    hobid:hobid};
+
+console.log(object);
+
+console.log(object.sünnipäev + '.' + object.sünnikuu + '.' + object.sünniaasta);
+
+console.log(`${object.sünnipäev}.${object.sünnikuu}.${object.sünniaasta}`);
+
+console.log(`Hobi 1: ${object.hobid[1]}`);
+
