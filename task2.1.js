38d5b235ce9a5c1a090a46ada0a02abab55ad4e5
Lauri Lauter <lauri.lauter@gmail.com>
2021-09-12 17:13:20 +0300

    'task2.1_done'

---
+++ task2.1.js
@@ -0,0 +1,23 @@
+let eesnimi ='Lauri'
+
+let bday = new Date(1980, 10, 06)
+
+let sünnipäev = bday.getDay()
+let sünnikuu = bday.getMonth()
+let sünniaasta = bday.geFullYear()
+
+
+let hobid = ['ballett','voolimine']
+
+let object = {
+    eesnimi:eesnimi,
+    sünnipäev:sünnipäev,
+    sünnikuu:sünnikuu,
+    sünniaasta:sünniaasta,
+    hobid:hobid}
+
+console.log(object)
+
+console.log(object.sünnipäev + '.' + object.sünnikuu + '.' + object.sünniaasta)
+
+console.log(`${object.sünnipäev}.${object.sünnikuu}.${object.sünniaasta}`)
