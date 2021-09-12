30ce70ab1954aaa99b0c7ca81058ee874c7af86b
Lauri <lauri.lauter@vikk.ee>
2021-09-12 22:08:16 +0300

    'task5-done'

---
+++ task5.js
@@ -0,0 +1,31 @@
+
+function check(string) {
+    if (!string) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function up(string) {
+    if (!string) {
+        return string
+    } else {
+        return string = string.charAt(0).toUpperCase() + string.slice(1);
+    }
+}
+
+const a = ''
+const b = 'tere'
+const c = 'minu nimi on John'
+
+
+console.log(check(a))
+console.log(check(b))
+console.log(check(c))
+
+console.log(up(a))
+console.log(up(b))
+console.log(up(c))
+
+
